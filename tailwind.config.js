7a3455908323b73094ac0829049d2745d8845a87
Rajan Prajapati <153183085+itsrajcode@users.noreply.github.com>
2024-04-06 21:51:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        customGray: '#16181c',
+        customWhite: '#E7E9EA',
+        buttonColor: '#1d9bf0',
+    },
+  },
+  plugins: [],
+}
+}
+
